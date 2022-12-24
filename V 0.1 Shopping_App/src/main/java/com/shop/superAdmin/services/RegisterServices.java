86f7360f99820c8e352ecbdacaaeb5df38c36c53
Shruti2906 <65824891+Shruti2906@users.noreply.github.com>
2022-12-24 11:08:30 -0800

    basic shopping application

---
+++ V 0.1 Shopping_App/src/main/java/com/shop/superAdmin/services/RegisterServices.java
@@ -0,0 +1,10 @@
+package com.shop.superAdmin.services;
+
+import java.util.List;
+
+import com.shop.model.Register;
+
+public interface RegisterServices {
+
+	int create(List<Register> lst);
+}
---
+++ V 0.1 Shopping_App/src/main/java/com/shop/user/controller/DisplayAllProdController.java
@@ -0,0 +1,43 @@
+package com.shop.user.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DisplayAllProdController
+ */
+@WebServlet("/DisplayAllProdController")
+public class DisplayAllProdController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DisplayAllProdController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println("display all prod controller");
+		
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
